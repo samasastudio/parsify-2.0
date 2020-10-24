368b86b8f51b67bbc6d15b8c71cccba01c700892
Sam Johnson <samj@iconcollective.com>
2020-10-24 17:49:15 -0500

    fixed header

--- parsify/src/App.js
+++ parsify/src/App.js
@@ -5,13 +5,13 @@ import {TextField, Button} from "@material-ui/core";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Parsify</h1>
+      <header><h1>Parsify</h1></header>
+      <div className="App-header">
         <form noValidate autoComplete="off">
           <TextField id="standard-basic" label="Standard" style={{width: '50vw'}}/>
           <Button>Search</Button>
         </form>
-      </header>
+      </div>
     </div>
   );
 }
