2d9175e956a06cb79b08b3a9628dbdf3be2d4659
Sam Johnson <samj@iconcollective.com>
2020-11-02 15:05:48 -0600

    added gradient styles and list items

--- parsify/src/App.js
+++ parsify/src/App.js
@@ -1,17 +1,47 @@
 import logo from "./logo.svg";
 import "./App.css";
-import {TextField, Button} from "@material-ui/core";
+import {TextField, Button, List, ListItem, ListItemText, Paper} from "@material-ui/core";
+import {FixedSizeList} from 'react-window';
 
 function App() {
   return (
     <div className="App">
-      <header><h1>Parsify</h1></header>
+      <header><h1>øøø</h1></header>
+      <Paper elevation={24}>
       <div className="App-view">
         <form noValidate autoComplete="off">
-          <TextField color="secondary" id="standard-basic" label="Enter Song" underlineStyle={{borderColor: '#D94A4A'}} style={{width: '50vw'}}/>
-          <Button>Search</Button>
+          <TextField label="Enter Song" variant="outlined" style={{width: '50vw', height: '50px'}}/>
+          <Button style={{marginLeft: '10px',marginBottom: '10px', height: '50px', border: '1px #d9a362 solid'}}>Search</Button>
         </form>
+        <List style={{width: '53vw'}}>
+          <ListItem style={{width: '53vw'}} button><ListItemText>Hello</ListItemText></ListItem>
+          <ListItem style={{width: '53vw'}} button><ListItemText>Hello</ListItemText></ListItem>
+          <ListItem style={{width: '53vw'}} button><ListItemText>Hello</ListItemText></ListItem>
+          <ListItem style={{width: '53vw'}} button><ListItemText>Hello</ListItemText></ListItem>
+          <ListItem style={{width: '53vw'}} button><ListItemText>Hello</ListItemText></ListItem>
+          <ListItem style={{width: '53vw'}} button><ListItemText>Hello</ListItemText></ListItem>
+          <ListItem style={{width: '53vw'}} button><ListItemText>Hello</ListItemText></ListItem>
+          <ListItem style={{width: '53vw'}} button><ListItemText>Hello</ListItemText></ListItem>
+          <ListItem style={{width: '53vw'}} button><ListItemText>Hello</ListItemText></ListItem>
+          <ListItem style={{width: '53vw'}} button><ListItemText>Hello</ListItemText></ListItem>
+          <ListItem style={{width: '53vw'}} button><ListItemText>Hello</ListItemText></ListItem>
+          <ListItem style={{width: '53vw'}} button><ListItemText>Hello</ListItemText></ListItem>
+          <ListItem style={{width: '53vw'}} button><ListItemText>Hello</ListItemText></ListItem>
+          <ListItem style={{width: '53vw'}} button><ListItemText>Hello</ListItemText></ListItem>
+          <ListItem style={{width: '53vw'}} button><ListItemText>Hello</ListItemText></ListItem>
+          <ListItem style={{width: '53vw'}} button><ListItemText>Hello</ListItemText></ListItem>
+          <ListItem style={{width: '53vw'}} button><ListItemText>Hello</ListItemText></ListItem>
+          <ListItem style={{width: '53vw'}} button><ListItemText>Hello</ListItemText></ListItem>
+          <ListItem style={{width: '53vw'}} button><ListItemText>Hello</ListItemText></ListItem>
+          <ListItem style={{width: '53vw'}} button><ListItemText>Hello</ListItemText></ListItem>
+          <ListItem style={{width: '53vw'}} button><ListItemText>Hello</ListItemText></ListItem>
+          <ListItem style={{width: '53vw'}} button><ListItemText>Hello</ListItemText></ListItem>
+          <ListItem style={{width: '53vw'}} button><ListItemText>Hello</ListItemText></ListItem>
+          <ListItem style={{width: '53vw'}} button><ListItemText>Hello</ListItemText></ListItem>
+          <ListItem style={{width: '53vw'}} button><ListItemText>Hello</ListItemText></ListItem>
+        </List>
       </div>
+      </Paper>
     </div>
   );
 }
--- parsify/src/index.js
+++ parsify/src/index.js
@@ -4,37 +4,37 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import {
-  unstable_createMuiStrictModeTheme,
+  createMuiTheme,
   ThemeProvider,
 } from "@material-ui/core/styles";
 import { dark } from "@material-ui/core/styles/createPalette";
 
-const theme = unstable_createMuiStrictModeTheme({
+const theme = createMuiTheme({
   palette: {
     type: 'dark',
     text: {
-      primary: '#D94A4A',
-      secondary: '#D94A4A'
+      primary: '#d9a362',
+      secondary: '#d9a362'
     },
     action: {
-      active: '#D94A4A',
+      active: '#d9a362',
     },
-    divider: '#D94A4A',
+    divider: '#d9a362',
     primary: {
-      main: '#D94A4A'
+      main: '#d9a362',
+      light: '#d9a362'
     },
     secondary: {
-      main: '#D94A4A'
+      main: '#d9a362',
+      light: '#d9a362'
     }
   },
 });
 
 ReactDOM.render(
-  <React.StrictMode>
     <ThemeProvider theme={theme}>
       <App />
-    </ThemeProvider>
-  </React.StrictMode>,
+    </ThemeProvider>,
   document.getElementById("root")
 );
 
