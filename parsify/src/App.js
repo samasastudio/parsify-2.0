73de30638aed9c48000e8b18a7a6c43a07732548
Sam Johnson <samj@iconcollective.com>
2020-11-04 11:43:58 -0600

    set up express server with ping

--- parsify/src/App.js
+++ parsify/src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import { Paper } from "@material-ui/core";
 import Search from "./components/Search";
 import { Pagination } from "@material-ui/lab";
+import axios from 'axios';
 
 function App() {
   const animateBackground = () => {
@@ -33,6 +34,10 @@ function App() {
   useEffect(() => {
     console.log("we're going to win");
     animateBackground();
+    axios.get('/ping')
+      .then(res => {
+        console.log(res.data)
+      })
   });
 
   return (
