74eb39d42ca73ba8044cfb7f202b95bd9944c732
Sam Johnson <samj@iconcollective.com>
2021-05-01 23:29:47 -0500

    removing configs

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  ID: '06917ff41d9b445cb07e51fcc4a33ce0',
-  SECRET: '89d632feefe14ddda7243c0a6c988c9e'
-}
--- db/mongoConfig.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  uri: `mongodb+srv://samasa:G!ter6247@cluster0.z0quc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-}
